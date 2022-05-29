1b6bbafbcf209b63a50378db841a64ad8e1f891b
salo322 <48085066+salo322@users.noreply.github.com>
2022-05-29 19:45:59 +0400

    update port

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.set("views", path.join(__dirname, "/views/"));
 app.engine("hbs", exphbs({ extname: "hbs", defaultLayout: "mainLayout", layoutsDir: __dirname + "/views/layouts/" }));
 app.set("view engine", "hbs");
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
    console.log("Express server started at port : 3000");
 });
 console.log("update");
